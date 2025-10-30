ab30aa0042f446b99122e0adee38aa781985a5de
Arina Zhukova <Azhukova19842005@yandex.ru>
2025-10-30 13:21:32 +0300

    Добавлен новый модуль helpers.js для функций-помощников. Добавлен паттерн анимации. Изменена анимация модального окна

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,3 +1,5 @@
+import { animate } from "./helpers";
+
 const modal = () => {
     const buttons = document.querySelectorAll('.popup-btn');
     const modal = document.querySelector('.popup'); 
@@ -14,23 +16,17 @@ const modal = () => {
                 modal.style.opacity = '0';
                 modalContent.style.transform = 'translateX(-50px)';
 
-                let startTime = null;
-                const duration = 200;
-
-                function animate(timestamp) {
-                    if (!startTime) startTime = timestamp;
-                    const progress = (timestamp - startTime) / duration;
-                    if (progress < 1) {
-                        modal.style.opacity = progress.toString();
-                        const translateX = -50 * (1 - progress);
-                        modalContent.style.transform = `translateX(${translateX}px)`;
-                        requestAnimationFrame(animate);
-                    } else {
-                        modal.style.opacity = '1';
-                        modalContent.style.transform = 'translateX(0)';
+                animate({
+                    duration: 300,
+                    timing(timeFraction) {
+                        return timeFraction;
+                    },
+                    draw(progress) {
+                        modal.style.opacity = progress;
                     }
-                }
-                requestAnimationFrame(animate);
+                });
+
+                
             }
         })
     })
